f7818cf70a76358123f47172709aa2f3cf3e75f2
Jus-Codin <70018166+Jus-Codin@users.noreply.github.com>
2022-09-27 09:28:10 +0800

    Update __init__.py

--- wolfram/__init__.py
+++ wolfram/__init__.py
@@ -1,8 +1,12 @@
 from wolfram.client import Client, AsyncClient
+from wolfram.params import Bool, LatLong, Units
 from wolfram import api
 
 __all__ = (
   Client,
   AsyncClient,
-  api
+  api,
+  Bool,
+  LatLong,
+  Units
 )
