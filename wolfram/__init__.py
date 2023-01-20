159e0baa2c1ba99ab533f8f759ebb50b183af5c0
Jus-Codin <justajusgamin@gmail.com>
2023-01-20 20:13:03 +0800

    Update __init__.py

--- wolfram/__init__.py
+++ wolfram/__init__.py
@@ -2,6 +2,11 @@ from wolfram.client import Client, AsyncClient
 from wolfram.params import Bool, LatLong, Units
 from wolfram import api
 
+__title__ = "wolfram.py"
+__author__ = "Jus-Codin"
+__license__ = "MIT"
+__version__ = "1.0.0"
+
 __all__ = (
   Client,
   AsyncClient,
